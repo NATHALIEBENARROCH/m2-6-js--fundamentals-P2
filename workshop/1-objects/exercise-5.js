40af542529a546b4802b5dfd04e6dae5b271baca
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-05-25 19:55:14 -0400

    workshop 6 finished

--- workshop/1-objects/exercise-5.js
+++ workshop/1-objects/exercise-5.js
@@ -21,71 +21,47 @@ const people = [
 // ------------
 // Write a function that returns the average age of the `people` array.
 
-//-------------------------------------------------
-function averageAge(peopleArr) {
-  ///!!!!!!!why arr
+// //-------------------------------------------------
+function averageAge(peopleArray) {
   let sumAges = 0;
-  const numberAges = peopleArr.length;
+  const numberAges = peopleArray.length;
 
-  peopleArr.forEach((person) => {
+  peopleArray.forEach((person) => {
     sumAges += person.age;
   });
-  return sumAges / numberAges;
+  return Math.round(sumAges / numberAges);
 }
 
 console.log(`The average age is ${averageAge(people)}.`);
+
 // Exercise 5.2
 // ------------
 // Write a function that, when passed an array of *people* (person objects) as
 // an argument, returns an array of their full names (each full name is a string).
 // Can you make use of your `fullName` function here?
-// TRIAL 0
-// people.forEach((person) => {
-//   if (person === " ") {
-//     console.log(person.name);
-//   }
-// });
-// LIKE BELOW
-// staffMembers.forEach((member) => {
-//   if (member.name === "Chuck Taylor") {
-//     console.log(member.skillLevels.javascript);
-//   }
-// });
-// TRIAL 1
-// console.log(Object.values(people.name));
-// TRIAL 2
-// function fullName(peopleArr) {
-//   // Your code here
-// function fullName(peopleArr) {
-//   const peopleNames = [];
-//   peopleArr.forEach((person) => {
-//     const fullName = Object.values(person.name).join(" ");
-//     peopleNames.push(fullName);
-//   });
 
-//   return peopleNames;
-// }
-// TRIAL 3
-// function fullNameMap(peopleArr) {
-//   return peopleArr.map((person) => {
-//     const allNames = Object.values(person.name);
-//     const fullNames = allNames.join(" ");
+let peoplemap = people.map((person) => {
+  return person.name.first + " " + person.name.middle + " " + person.name.last;
+});
+console.log(peoplemap);
 
-//     return fullNames;
-//   });
-// }
-// console.log(fullNameMap(people));
+// or
+
+function fullNameMap(peopleArr) {
+  return peopleArr.map((person) => {
+    const allNames = Object.values(person.name);
+    const fullNames = allNames.join(" ");
+
+    return fullNames;
+  });
+}
+console.log(fullNameMap(people));
 //-------------------------------------------------
 
 // Exercise 5.3
 // ------------
 // Write a function that, when given *people* and an *age* as arguments,
 // // returns an array of just the people that are older than the specified age..
-// MY START
-// function olderPeople(peopleArr, age);
-//   if (people.age > 26)
-//   return
-// Your code here
 
 function olderPeople(peopleArr, age) {
   return peopleArr.filter((person) => {
